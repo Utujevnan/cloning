918380583f6972488ad7a11ef8e5b3f922fd3b7d
Vanessa Utuje <utujevan@gmail.com>
2025-02-12 14:34:10 +0200

    Initial commit

---
+++ .vscode/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',        // Adjust this to match the location of your HTML file
+      './src/**/*.{js,ts,jsx,tsx}',  // Include any files in the src folder
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
